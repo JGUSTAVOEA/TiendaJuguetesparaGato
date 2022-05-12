a49d1e7336a4f18a7d2720f344400899706604ad
luistrejo-dev <L20280734@toluca.tecnm.mx>
2022-05-12 00:36:04 -0500

    Interfaz Login y Registros

--- app/src/main/java/com/example/tiendajuguetesparagato/LoginActivity.kt
+++ app/src/main/java/com/example/tiendajuguetesparagato/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tiendajuguetesparagato
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn = findViewById<Button>(R.id.btnRegistrar)
+        btn.setOnClickListener{
+            val lanzar = Intent(this, RegistroActivity::class.java)
+            startActivity(lanzar)
+        }
+
     }
 }
